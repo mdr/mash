70515d5aaf52790e272606f0195f037576593374
Matt Russell <MattRussellUK@gmail.com>
2016-05-05 20:37:39 +0100

    git.add has file completion arguments

--- src/main/scala/com/github/mdr/mash/ns/git/AddFunction.scala
+++ src/main/scala/com/github/mdr/mash/ns/git/AddFunction.scala
@@ -14,6 +14,8 @@ import com.github.mdr.mash.evaluator.ToStringifier
 import com.github.mdr.mash.ns.core.UnitClass
 import com.github.mdr.mash.functions.FunctionHelpers
 import java.nio.file.Path
+import com.github.mdr.mash.inference.TypedArguments
+import com.github.mdr.mash.completions.CompletionSpec
 
 object AddFunction extends MashFunction("git.add") {
 
@@ -32,13 +34,15 @@ object AddFunction extends MashFunction("git.add") {
     val boundParams = params.validate(arguments)
     val argItems = boundParams(Paths).asInstanceOf[MashList].items
     val paths = FunctionHelpers.interpretAsPaths(boundParams(Paths))
-    GitHelper.withGit { git =>
+    GitHelper.withGit { git ⇒
       val cmd = git.add
       for (path ← paths)
         cmd.addFilepattern(path.toString)
       cmd.call()
     }
   }
+
+  override def getCompletionSpecs(argPos: Int, arguments: TypedArguments) = Seq(CompletionSpec.File)
 
   override def typeInferenceStrategy = ConstantTypeInferenceStrategy(Type.Instance(UnitClass))
 
