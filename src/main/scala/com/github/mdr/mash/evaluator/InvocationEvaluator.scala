9d4282a9130133739eddd1232e287fd239c8314f
Matt Russell <MattRussellUK@gmail.com>
2016-06-20 20:51:36 +0100

    SourceLocation refactor

--- src/main/scala/com/github/mdr/mash/evaluator/InvocationEvaluator.scala
+++ src/main/scala/com/github/mdr/mash/evaluator/InvocationEvaluator.scala
@@ -31,44 +31,42 @@ object InvocationEvaluator extends EvaluatorHelper {
     case Argument.LongFlag(flag, valueOpt, sourceInfoOpt) ⇒ EvaluatedArgument.LongFlag(flag, valueOpt.map(v ⇒ Evaluator.evaluate(v)), Some(arg))
   }
 
-  private def callFunction(function: MashValue, arguments: Arguments, functionExpr: Expr, invocationExpr: Expr): MashValue =
-    callFunction(function, arguments, Some(functionExpr), Some(invocationExpr))
+  private def callFunction(function: MashValue, arguments: Arguments, functionExpr: Expr, invocationExpr: Expr)(implicit context: EvaluationContext): MashValue =
+    callFunction(function, arguments, sourceLocation(functionExpr), sourceLocation(invocationExpr))
 
-  def callFunction(function: MashValue, arguments: Arguments, functionExprOpt: Option[Expr] = None, invocationExprOpt: Option[Expr] = None): MashValue = {
-    val functionLocationOpt = functionExprOpt.flatMap(_.locationOpt)
-    val invocationLocationOpt = invocationExprOpt.flatMap(_.locationOpt)
+  def callFunction(function: MashValue, arguments: Arguments, functionLocationOpt: Option[SourceLocation] = None, invocationLocationOpt: Option[SourceLocation] = None): MashValue = {
     function match {
       case MashString(memberName, _) ⇒
         callStringAsFunction(memberName, arguments, functionLocationOpt, invocationLocationOpt)
       case f: MashFunction ⇒
-        Evaluator.addLocationToExceptionIfMissing(invocationLocationOpt) {
+        Evaluator.addLocationToExceptionIfMissing(invocationLocationOpt.map(_.pointedRegion)) {
           f(arguments)
         }
       case BoundMethod(target, method, _) ⇒
-        Evaluator.addLocationToExceptionIfMissing(invocationLocationOpt) {
+        Evaluator.addLocationToExceptionIfMissing(invocationLocationOpt.map(_.pointedRegion)) {
           method(target, arguments)
         }
       case _ ⇒
-        throw EvaluatorException(s"Not callable", functionLocationOpt.map(SourceLocation))
+        throw EvaluatorException(s"Not callable", functionLocationOpt)
     }
   }
 
-  private def callStringAsFunction(s: String, arguments: Arguments, functionLocationOpt: Option[PointedRegion], invocationLocationOpt: Option[PointedRegion]): MashValue =
+  private def callStringAsFunction(s: String, arguments: Arguments, functionLocationOpt: Option[SourceLocation], invocationLocationOpt: Option[SourceLocation]): MashValue =
     arguments.positionArgs match {
       case Seq(EvaluatedArgument.PositionArg(xs: MashList, _)) ⇒
         xs.map { target ⇒
-          val intermediateResult = MemberEvaluator.lookup(target, s, functionLocationOpt)
-          Evaluator.addLocationToExceptionIfMissing(invocationLocationOpt) {
+          val intermediateResult = MemberEvaluator.lookup(target, s, functionLocationOpt.map(_.pointedRegion))
+          Evaluator.addLocationToExceptionIfMissing(invocationLocationOpt.map(_.pointedRegion)) {
             Evaluator.immediatelyResolveNullaryFunctions(intermediateResult)
           }
         }
       case Seq(EvaluatedArgument.PositionArg(target, _)) ⇒
-        val intermediateResult = MemberEvaluator.lookup(target, s, functionLocationOpt)
-        Evaluator.addLocationToExceptionIfMissing(invocationLocationOpt) {
+        val intermediateResult = MemberEvaluator.lookup(target, s, functionLocationOpt.map(_.pointedRegion))
+        Evaluator.addLocationToExceptionIfMissing(invocationLocationOpt.map(_.pointedRegion)) {
           Evaluator.immediatelyResolveNullaryFunctions(intermediateResult)
         }
       case _ ⇒
-        throw EvaluatorException(s"Cannot call a String on multiple arguments", invocationLocationOpt.map(SourceLocation))
+        throw EvaluatorException(s"Cannot call a String on multiple arguments", invocationLocationOpt)
     }
 
 }
