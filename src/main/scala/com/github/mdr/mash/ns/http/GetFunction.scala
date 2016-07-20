a9ce3ae775f1c867e6c395f7047991988f950bbf
Matt Russell <MattRussellUK@gmail.com>
2016-07-20 17:55:40 +0100

    Add --basicAuth option to http.get

--- src/main/scala/com/github/mdr/mash/ns/http/GetFunction.scala
+++ src/main/scala/com/github/mdr/mash/ns/http/GetFunction.scala
@@ -17,6 +17,13 @@ import org.apache.commons.io.IOUtils
 import scala.collection.immutable.ListMap
 import com.github.mdr.mash.runtime.MashNumber
 import com.github.mdr.mash.runtime.MashString
+import com.github.mdr.mash.runtime.MashNull
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.apache.http.impl.client.BasicCredentialsProvider
+import org.apache.http.auth.AuthScope
+import org.apache.http.impl.client.HttpClientBuilder
+
+case class BasicCredentials(username: String, password: String)
 
 object GetFunction extends MashFunction("http.get") {
 
@@ -24,20 +31,41 @@ object GetFunction extends MashFunction("http.get") {
     val Url = Parameter(
       name = "url",
       summary = "URL to fetch")
+    val BasicAuth = Parameter(
+      name = "basicAuth",
+      summary = "Basic authentication",
+      defaultValueGeneratorOpt = Some(() ⇒ MashNull))
   }
   import Params._
 
-  val params = ParameterModel(Seq(Url))
+  val params = ParameterModel(Seq(Url, BasicAuth))
+
+  private def getBasicCredentials(arg: MashValue): Option[BasicCredentials] =
+    MashNull.option(arg).map {
+      case MashString(s, _) ⇒
+        val Seq(username, password) = s.split(":").take(2).toSeq
+        BasicCredentials(username, password)
+      case obj: MashObject ⇒
+        BasicCredentials(obj("username").asInstanceOf[MashString].s, obj("password").asInstanceOf[MashString].s)
+    }
 
   def apply(arguments: Arguments): MashObject = {
     val boundParams = params.validate(arguments)
     val url = boundParams.validateString(Url)
-    val httpclient = HttpClients.createDefault
+
+    val basicCredentialsOpt = getBasicCredentials(boundParams(BasicAuth))
+    val credentialsProvider = new BasicCredentialsProvider
+    val clientBuilder = HttpClientBuilder.create()
+    for (BasicCredentials(username, password) ← basicCredentialsOpt) {
+      credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password))
+      clientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+    }
+    val client = clientBuilder.build()
     val httpGet = new HttpGet(new URI(url.s))
-    val response = httpclient.execute(httpGet)
+    val response = client.execute(httpGet)
     val code = response.getStatusLine.getStatusCode
-    val is = response.getEntity.getContent
-    val body = IOUtils.toString(is, "UTF-8")
+    val content = response.getEntity.getContent
+    val body = IOUtils.toString(content, "UTF-8")
     import ResponseClass.Fields._
     MashObject.of(ListMap(
       Status -> MashNumber(code),
@@ -47,8 +75,6 @@ object GetFunction extends MashFunction("http.get") {
 
   override def typeInferenceStrategy = ConstantTypeInferenceStrategy(ResponseClass)
 
-  override def getCompletionSpecs(argPos: Int, arguments: TypedArguments) = Seq(CompletionSpec.File)
-
-  override def summary = "Make an HTTP request "
+  override def summary = "Make an HTTP GET request"
 
 }
