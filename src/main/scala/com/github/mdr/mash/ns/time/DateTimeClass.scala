bcab5ae2bd3c83022011544d3d0bd6c31c508e17
Matt Russell <MattRussellUK@gmail.com>
2016-12-01 12:44:55 +0000

    Add DateTime.fuzzy

--- src/main/scala/com/github/mdr/mash/ns/time/DateTimeClass.scala
+++ src/main/scala/com/github/mdr/mash/ns/time/DateTimeClass.scala
@@ -1,15 +1,17 @@
 package com.github.mdr.mash.ns.time
 
 import java.time._
+import java.util.Date
 
 import com.github.mdr.mash.evaluator._
 import com.github.mdr.mash.functions.{ MashMethod, ParameterModel }
-import com.github.mdr.mash.ns.core.{ AnyClass, NumberClass }
+import com.github.mdr.mash.ns.core.{ AnyClass, NumberClass, StringClass }
 import com.github.mdr.mash.runtime._
+import org.ocpsoft.prettytime.PrettyTime
 
 object DateTimeClass extends MashClass("time.DateTime") {
 
-  override val methods = Seq(DateMethod, MillisSinceEpoch)
+  override val methods = Seq(DateMethod, FuzzyMethod, MillisSinceEpoch)
 
   case class Wrapper(target: MashValue) {
 
@@ -31,6 +33,22 @@ object DateTimeClass extends MashClass("time.DateTime") {
     override def typeInferenceStrategy = NumberClass
 
     override def summary = "Date portion of this date and time"
+
+  }
+
+  object FuzzyMethod extends MashMethod("fuzzy") {
+    private val prettyTime = new PrettyTime
+
+    val params = ParameterModel()
+
+    def apply(target: MashValue, arguments: Arguments): MashString = {
+      params.validate(arguments)
+      MashString(prettyTime.format(Date.from(Wrapper(target).instant)))
+    }
+
+    override def typeInferenceStrategy = StringClass
+
+    override def summary = "Render as human-friendly relative time, e.g. 1 day ago, 3 months from now, etc"
 
   }
 
