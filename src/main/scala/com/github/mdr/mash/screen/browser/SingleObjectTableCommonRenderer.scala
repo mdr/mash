cc8415fea6d82b729d06a4d6bd89272d57bc766c
Matt Russell <MattRussellUK@gmail.com>
2017-02-06 20:56:12 +0000

    Tidy up single table object rendering

--- src/main/scala/com/github/mdr/mash/screen/browser/SingleObjectTableCommonRenderer.scala
+++ src/main/scala/com/github/mdr/mash/screen/browser/SingleObjectTableCommonRenderer.scala
@@ -11,15 +11,57 @@ import com.github.mdr.mash.utils.Utils._
 import scala.collection.mutable.ArrayBuffer
 
 class SingleObjectTableCommonRenderer(model: SingleObjectTableModel, terminalInfo: TerminalInfo) {
+
   private val boxCharacterSupplier = UnicodeBoxCharacterSupplier
 
-  import boxCharacterSupplier._
+  def renderTableLines(fieldValuePairs: Seq[(String, String)],
+                       selectedIndexOpt: Option[Int] = None,
+                       moreDataItemsAboveWindow: Boolean = false,
+                       moreDataItemsBelowWindow: Boolean = false): Seq[Line] =
+    renderHeaderLines(moreDataItemsAboveWindow) ++
+      renderFieldLines(fieldValuePairs, selectedIndexOpt) ++
+      Seq(renderFooterLine(break = hasFields, addArrow = moreDataItemsBelowWindow))
 
-  private def fullRowWidth = model.fieldColumnWidth + model.valueColumnWidth + 1
+  private def renderHeaderLines(moreDataItemsAboveWindow: Boolean): Seq[Line] =
+    model.classNameOpt match {
+      case Some(className) ⇒ renderClassHeaderLines(className, moreDataItemsAboveWindow)
+      case None            ⇒ Seq(renderTopRow(break = hasFields, addArrow = moreDataItemsAboveWindow))
+    }
 
+  private def renderClassHeaderLines(className: String, moreDataItemsAboveWindow: Boolean): Seq[Line] = {
+    val topLine = renderTopRow(break = false, addArrow = false)
+    val classNameLine = renderClassNameLine(className)
+    val belowHeaderLineOpt =
+      if (model.fields.isEmpty)
+        None
+      else
+        Some(renderBelowHeaderLine(addArrow = moreDataItemsAboveWindow))
+    Seq(topLine, classNameLine) ++ belowHeaderLineOpt
+  }
+
+  /**
+    * ╔══════════════════════════╗
+    * or
+    * ╔════════════╤═════════════╗
+    */
+  private def renderTopRow(break: Boolean = true, addArrow: Boolean): Line = {
+    import boxCharacterSupplier._
+    val chars = new StringBuilder()
+      .append(doubleTopLeft)
+      .append(doubleHorizontal * model.fieldColumnWidth)
+      .append(if (break) doubleHorizontalSingleDown else doubleHorizontal)
+      .append(doubleHorizontal * model.valueColumnWidth)
+      .append(doubleTopRight)
+      .toString
+    Line(chars.when(addArrow, addUpArrow).style)
+  }
+
+  /**
+    * ║           Class          ║
+    **/
   private def renderClassNameLine(className: String): Line = {
     val fit = StringUtils.ellipsisise(StringUtils.centre(className, fullRowWidth), fullRowWidth)
-    val renderedClassName = fit.style(Style(bold = true, foregroundColour = Colour.Yellow))
+    val renderedClassName = fit.style(classNameStyle)
     val buffer = ArrayBuffer[StyledCharacter]()
     buffer ++= boxCharacterSupplier.doubleVertical.style
     buffer ++= renderedClassName
@@ -27,19 +69,53 @@ class SingleObjectTableCommonRenderer(model: SingleObjectTableModel, terminalInf
     Line(buffer)
   }
 
-  private def renderHeaderLines(moreDataItemsAboveWindow: Boolean): Seq[Line] = {
-    model.classNameOpt match {
-      case Some(className) ⇒
-        val topLine = Line(renderTopRow(model, break = false).style)
-        val classNameLine = renderClassNameLine(className)
-        val belowHeaderChars = renderBelowHeaderChars
-        val belowHeaderLine = Line(belowHeaderChars.when(moreDataItemsAboveWindow, addUpArrow).style)
-        Seq(topLine, classNameLine, belowHeaderLine)
-      case None            ⇒
-        val topLineChars = renderTopRow(model)
-        val newTopLineChars = topLineChars.when(moreDataItemsAboveWindow, addUpArrow)
-        Seq(Line(newTopLineChars.style))
-    }
+  /**
+    * ╟────────────┬─────────────╢
+    */
+  private def renderBelowHeaderLine(addArrow: Boolean): Line = {
+    import boxCharacterSupplier._
+    val chars = new StringBuilder()
+      .append(doubleVerticalSingleRight)
+      .append(singleHorizontal * model.fieldColumnWidth)
+      .append(singleHorizontalSingleDown)
+      .append(singleHorizontal * model.valueColumnWidth)
+      .append(doubleVerticalSingleLeft)
+      .toString
+    Line(chars.when(addArrow, addUpArrow).style)
+  }
+
+  private def renderFieldLines(fieldValuePairs: Seq[(String, String)], selectedIndexOpt: Option[Int]): Seq[Line] =
+    for {
+      ((renderedField, renderedValue), i) ← fieldValuePairs.zipWithIndex
+      isCursorRow = selectedIndexOpt contains i
+    } yield renderFieldLine(renderedField, renderedValue, isCursorRow)
+
+  /**
+    * ║fieldName   │fieldValue   ║
+    */
+  private def renderFieldLine(renderedField: String, renderedValue: String, isCursorRow: Boolean): Line = {
+    val side = boxCharacterSupplier.doubleVertical.style
+    val internalVertical = boxCharacterSupplier.singleVertical.style(internalRowStyle(isCursorRow))
+    val fieldChars = StringUtils.fitToWidth(renderedField, model.fieldColumnWidth).style(fieldStyle(isCursorRow))
+    val valueChars = StringUtils.fitToWidth(renderedValue, model.valueColumnWidth).style(internalRowStyle(isCursorRow))
+    Line(side ++ fieldChars ++ internalVertical ++ valueChars ++ side)
+  }
+
+  /**
+    * ╚════════════╧═════════════╝
+    * or
+    * ╚══════════════════════════╝
+    */
+  private def renderFooterLine(break: Boolean, addArrow: Boolean): Line = {
+    import boxCharacterSupplier._
+    val chars = new StringBuilder()
+      .append(doubleBottomLeft)
+      .append(doubleHorizontal * model.fieldColumnWidth)
+      .append(if (break) doubleHorizontalSingleUp else doubleHorizontal)
+      .append(doubleHorizontal * model.valueColumnWidth)
+      .append(doubleBottomRight)
+      .toString
+    Line(chars.when(addArrow, addDownArrow).style)
   }
 
   private def addUpArrow(s: String): String = setMiddleCharacter(s, '↑')
@@ -49,84 +125,14 @@ class SingleObjectTableCommonRenderer(model: SingleObjectTableModel, terminalInf
   private def setMiddleCharacter(s: String, c: Char): String =
     if (s.isEmpty) s else s.updated(s.length / 2, c)
 
-  private def renderFooterLine(moreDataItemsBelowWindow: Boolean) = {
-    val bottomRow = renderBottomRow(model)
-    val newBottom = bottomRow.when(moreDataItemsBelowWindow, addDownArrow)
-    Line(newBottom.style)
-  }
+  private def fullRowWidth = model.fieldColumnWidth + model.valueColumnWidth + 1
 
-  private def renderRow(renderedField: String, renderedValue: String, isCursorRow: Boolean): Line = {
-    val side = boxCharacterSupplier.doubleVertical.style
-    val internalVertical = boxCharacterSupplier.singleVertical.style(Style(inverse = isCursorRow))
-    val fieldStyle = Style(inverse = isCursorRow, foregroundColour = Colour.Yellow)
-    val valueStyle = Style(inverse = isCursorRow)
-    val fieldChars = StringUtils.fitToWidth(renderedField, model.fieldColumnWidth).style(fieldStyle)
-    val valueChars = StringUtils.fitToWidth(renderedValue, model.valueColumnWidth).style(valueStyle)
-    Line(side ++ fieldChars ++ internalVertical ++ valueChars ++ side)
-  }
+  private def hasFields: Boolean = model.fields.nonEmpty
 
-  private def renderDataLines(fieldValuePairs: Seq[(String, String)], selectedIndexOpt: Option[Int]): Seq[Line] =
-    for {
-      ((renderedField, renderedValue), i) ← fieldValuePairs.zipWithIndex
-    } yield renderRow(renderedField, renderedValue, isCursorRow = selectedIndexOpt contains i)
+  private val classNameStyle: Style = Style(bold = true, foregroundColour = Colour.Yellow)
 
-  def renderTableLines(fieldValuePairs: Seq[(String, String)],
-                       selectedIndexOpt: Option[Int] = None,
-                       moreDataItemsAboveWindow: Boolean = false,
-                       moreDataItemsBelowWindow: Boolean = false): Seq[Line] =
-    renderHeaderLines(moreDataItemsAboveWindow) ++
-      renderDataLines(fieldValuePairs, selectedIndexOpt) ++
-      Seq(renderFooterLine(moreDataItemsBelowWindow))
+  private def internalRowStyle(isCursorRow: Boolean): Style = Style(inverse = isCursorRow)
 
-
-  /** ╔══════════════════════════╗
-    * or
-    * ╔════════════╤═════════════╗
-    */
-  private def renderTopRow(model: SingleObjectTableModel, break: Boolean = true): String =
-    new StringBuilder()
-      .append(doubleTopLeft)
-      .append(doubleHorizontal * model.fieldColumnWidth)
-      .append(if (break) doubleHorizontalSingleDown else doubleHorizontal)
-      .append(doubleHorizontal * model.valueColumnWidth)
-      .append(doubleTopRight)
-      .toString
-
-  /**
-    * ║type        │file         ║
-    */
-  private def renderFieldRow(renderedField: String, renderedValue: String): String =
-    new StringBuilder()
-      .append(doubleVertical)
-      .append(renderedField)
-      .append(singleVertical)
-      .append(renderedValue)
-      .append(doubleVertical)
-      .toString
-
-  /**
-    * ╚════════════╧═════════════╝
-    */
-  private def renderBottomRow(model: SingleObjectTableModel): String =
-    new StringBuilder()
-      .append(doubleBottomLeft)
-      .append(doubleHorizontal * model.fieldColumnWidth)
-      .append(doubleHorizontalSingleUp)
-      .append(doubleHorizontal * model.valueColumnWidth)
-      .append(doubleBottomRight)
-      .toString
-
-  /**
-    * ╟────────────┬─────────────╢
-    */
-  private def renderBelowHeaderChars: String = {
-    val sb = new StringBuilder()
-    sb.append(doubleVerticalSingleRight)
-    sb.append(singleHorizontal * model.fieldColumnWidth)
-    sb.append(singleHorizontalSingleDown)
-    sb.append(singleHorizontal * model.valueColumnWidth)
-    sb.append(doubleVerticalSingleLeft)
-    sb.toString
-  }
+  private def fieldStyle(isCursorRow: Boolean): Style = Style(inverse = isCursorRow, foregroundColour = Colour.Yellow)
 
 }
