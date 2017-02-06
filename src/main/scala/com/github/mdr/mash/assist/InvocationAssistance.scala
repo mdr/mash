aceb85369a7c3964be5f976b1ec2fa508ed1a751
Matt Russell <MattRussellUK@gmail.com>
2017-02-06 08:22:20 +0000

    Tidy InvocationAssistance

--- src/main/scala/com/github/mdr/mash/assist/InvocationAssistance.scala
+++ src/main/scala/com/github/mdr/mash/assist/InvocationAssistance.scala
@@ -8,6 +8,7 @@ import com.github.mdr.mash.lexer.{ MashLexer, Token }
 import com.github.mdr.mash.parser.AbstractSyntax._
 import com.github.mdr.mash.parser.ConcreteSyntax
 import com.github.mdr.mash.runtime.MashValue
+import com.github.mdr.mash.utils.Utils._
 import com.github.mdr.mash.utils.{ Region, Utils }
 
 import scala.PartialFunction._
@@ -19,23 +20,17 @@ object InvocationAssistance {
                                           bindings: Map[String, MashValue],
                                           mish: Boolean): Option[AssistanceState] = {
     val tokens = MashLexer.tokenise(s, forgiving = true, mish = mish).rawTokens
-    val expr = Compiler.compileForgiving(CompilationUnit(s, mish = mish), bindings, CompilationSettings(inferTypes = true)).body
+    val expr = Compiler.compileForgiving(CompilationUnit(s, mish = mish), bindings, CompilationSettings(inferTypes = true))
     for {
-      sourceInfo ← expr.sourceInfoOpt
       invocationExpr ← findInnermostInvocationContaining(expr, tokens, pos)
-      (functionTypeOpt, valueOpt) = getFunctionType(invocationExpr)
-      functionType ← functionTypeOpt
+      functionType ← getFunctionType(invocationExpr)
       assistanceState ← assistInvocation(functionType)
     } yield assistanceState
   }
 
-  private def getFunctionType(expr: Expr): (Option[Type], Option[MashValue]) = expr match {
-    case InvocationExpr(f, _, _, _) ⇒ (f.typeOpt, f.constantValueOpt)
-    case _                          ⇒ (expr.preInvocationTypeOpt orElse expr.typeOpt, None)
-  }
-
-  private def assistInvocation(function: MashValue): Option[AssistanceState] = function match {
-    case _ ⇒ None
+  private def getFunctionType(expr: Expr): Option[Type] = expr match {
+    case InvocationExpr(f, _, _, _) ⇒ f.typeOpt
+    case _                          ⇒ expr.preInvocationTypeOpt orElse expr.typeOpt
   }
 
   private def assistInvocation(functionType: Type): Option[AssistanceState] = functionType match {
@@ -66,57 +61,43 @@ object InvocationAssistance {
     case _                                                                 ⇒ None
   }
 
-  def callingSyntax(funOrMethod: Any): String =
+  private def callingSyntax(funOrMethod: Any): String =
     funOrMethod match {
       case f: MashFunction ⇒ f.name + " " + f.params.callingSyntax
       case bm: BoundMethod ⇒ bm.method.name + " " + bm.method.params.callingSyntax
       case m: MashMethod   ⇒ m.name + " " + m.params.callingSyntax
     }
 
-  private def findInnermostInvocationContaining(expr: Expr, tokens: Seq[Token], pos: Int): Option[Expr] = {
-    val enclosingInvocations = expr.findAll {
-      case e: InvocationExpr if expandedRegionContains(e, tokens, pos)                           ⇒ e
-      case e: Expr if e.preInvocationTypeOpt.isDefined && expandedRegionContains(e, tokens, pos) ⇒ e
-      case e: Expr if hasFunctionType(e) && expandedRegionContains(e, tokens, pos)               ⇒ e
+  private def findInnermostInvocationContaining(program: AstNode, tokens: Seq[Token], pos: Int): Option[Expr] = {
+    val enclosingInvocations = program.findAll {
+      case expr: InvocationExpr if expandedRegionContains(expr, tokens, pos)                              ⇒ expr
+      case expr: Expr if expr.preInvocationTypeOpt.isDefined && expandedRegionContains(expr, tokens, pos) ⇒ expr
     }
     def size(expr: Expr): Int = expr.sourceInfoOpt.map(_.node.region.length).getOrElse(Integer.MAX_VALUE)
     Utils.minBy(enclosingInvocations, size)
   }
 
-  private def hasFunctionType(e: Expr): Boolean =
-    e.typeOpt.exists(cond(_) {
-      case Type.BuiltinFunction(_) | Type.BoundBuiltinMethod(_, _)
-           | _: Type.UserDefinedFunction | _: Type.BoundUserDefinedMethod ⇒ true
-    })
-
   private def expandedRegionContains(expr: Expr, tokens: Seq[Token], pos: Int): Boolean =
     expr.sourceInfoOpt.exists(info ⇒ rightExpandedRegion(info.node, tokens) contains pos)
 
-  private def rightExpandedRegion(expr: ConcreteSyntax.AstNode, tokens: Seq[Token]): Region = {
-    val rightmostTokenOpt = expr.tokens.lastOption.map { lastToken ⇒
-      val tokensAfterLast = getTokensAfterLast(lastToken, tokens)
+  /**
+    * Expand the region of the node to include any whitespace / comment / EOF tokens to the right of it.
+    */
+  private def rightExpandedRegion(node: ConcreteSyntax.AstNode, allTokens: Seq[Token]): Region = {
+    val rightmostTokenOpt = node.tokens.lastOption.map { lastToken ⇒
+      val tokensAfterLast = getTokensAfterLast(lastToken, allTokens)
       findRightmostToken(tokensAfterLast).getOrElse(lastToken)
     }
-    def getRegion(token: Token) = if (token.isEof) token.region.grow(1) else token.region
-    rightmostTokenOpt.map(getRegion).getOrElse(Region(0, 0)) merge expr.region
+    def getRegion(token: Token) = token.region.when(token.isEof, _ grow 1)
+    rightmostTokenOpt.map(getRegion) getOrElse Region(0, 0) merge node.region
   }
 
   private def getTokensAfterLast(lastToken: Token, remainingTokens: Seq[Token]): Seq[Token] =
-    remainingTokens match {
-      case Seq(head, rest@_*) ⇒
-        if (head == lastToken)
-          rest
-        else
-          getTokensAfterLast(lastToken, rest)
-      case Seq()              ⇒
-        Seq()
-    }
+    Utils.indexOf(remainingTokens, lastToken)
+      .map(i => remainingTokens.drop(i + 1))
+      .getOrElse(Seq())
 
   private def findRightmostToken(remainingTokens: Seq[Token]): Option[Token] =
-    remainingTokens match {
-      case Seq(head, rest@_*) if head.isWhitespace || head.isComment || head.isEof ⇒
-        findRightmostToken(rest) orElse Some(head)
-      case _                                                                       ⇒
-        None
-    }
+    remainingTokens.takeWhile(token ⇒ token.isWhitespace || token.isComment || token.isEof).headOption
+
 }
