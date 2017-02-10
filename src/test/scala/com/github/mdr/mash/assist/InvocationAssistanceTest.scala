614e11311ba790be4332c30d81e2ac0090da78a8
Matt Russell <MattRussellUK@gmail.com>
2017-02-10 21:08:10 +0000

    Add more invocation assistance test cases

--- src/test/scala/com/github/mdr/mash/assist/InvocationAssistanceTest.scala
+++ src/test/scala/com/github/mdr/mash/assist/InvocationAssistanceTest.scala
@@ -3,7 +3,9 @@ package com.github.mdr.mash.assist
 import com.github.mdr.mash.assist.InvocationAssistance.getTypeOfNearestFunction
 import com.github.mdr.mash.evaluator.StandardEnvironment
 import com.github.mdr.mash.inference.Type
-import com.github.mdr.mash.ns.os.ListFilesFunction
+import com.github.mdr.mash.ns.collections.{ ListClass, ReverseFunction }
+import com.github.mdr.mash.ns.core.{ AnyClass, NumberClass, StringClass }
+import com.github.mdr.mash.ns.os.{ HomeFunction, ListFilesFunction }
 import com.github.mdr.mash.repl.LineBufferTestHelper
 import org.scalatest.{ FlatSpec, Matchers }
 
@@ -15,6 +17,18 @@ class InvocationAssistanceTest extends FlatSpec with Matchers {
   "ls ▶" ==> Type.BuiltinFunction(ListFilesFunction)
   "ls 'dir'▶" ==> Type.BuiltinFunction(ListFilesFunction)
   "ls --recursive▶" ==> Type.BuiltinFunction(ListFilesFunction)
+
+  "ls home▶" ==> Type.BuiltinFunction(HomeFunction)
+  "ls ▶home" ==> Type.BuiltinFunction(HomeFunction)
+  "ls▶ home" ==> Type.BuiltinFunction(ListFilesFunction)
+
+  "ls (▶home)" ==> Type.BuiltinFunction(HomeFunction)
+  "ls ▶(home)" ==> Type.BuiltinFunction(ListFilesFunction)
+
+  "reverse▶" ==> Type.BuiltinFunction(ReverseFunction)
+
+  "1.toString▶" ==> Type.BoundBuiltinMethod(NumberClass, AnyClass.ToStringMethod)
+  "''.startsWith 'x'▶" ==> Type.BoundBuiltinMethod(StringClass, StringClass.StartsWithMethod)
 
   implicit class RichString(s: String) {
 
