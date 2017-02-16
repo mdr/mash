7aa99010aa6e56e908f9e52a165b3582cfc2b9f1
Matt Russell <MattRussellUK@gmail.com>
2017-02-16 18:21:47 +0000

    Suppress commons logging output (to shut up Apache httpclient)

--- src/main/scala/com/github/mdr/mash/ns/core/TimedResultClass.scala
+++ src/main/scala/com/github/mdr/mash/ns/core/TimedResultClass.scala
@@ -5,7 +5,7 @@ import java.time.Instant
 import com.github.mdr.mash.classes.{ Field, MashClass, NewStaticMethod }
 import com.github.mdr.mash.evaluator.Arguments
 import com.github.mdr.mash.functions.{ MashMethod, ParameterModel }
-import com.github.mdr.mash.inference.{ ConstantMethodTypeInferenceStrategy, Type }
+import com.github.mdr.mash.inference.Type
 import com.github.mdr.mash.inference.Type.classToType
 import com.github.mdr.mash.ns.time.{ DateTimeClass, MillisecondsClass }
 import com.github.mdr.mash.runtime._
