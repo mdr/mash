a0f377d58d7390307d026763f0d53785ffdb7c28
Matt Russell <MattRussellUK@gmail.com>
2017-03-08 07:11:05 +0000

    Update @namedArgs tests

--- src/test/scala/com/github/mdr/mash/evaluator/EvaluatorTest.scala
+++ src/test/scala/com/github/mdr/mash/evaluator/EvaluatorTest.scala
@@ -968,7 +968,6 @@ class EvaluatorTest extends AbstractEvaluatorTest {
   // @namedArgs
   "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject --foo=1" ==> "{ foo: 1 }"
   "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject --foo" ==> "{ foo: true }"
-  "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject --namedArgs=1" ==> "{ namedArgs: 1}"
   "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject -a" ==> "{ a: true }"
   "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject -abc" ==> "{ a: true, b: true, c: true }"
   "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject" ==> "{}"
@@ -977,13 +976,17 @@ class EvaluatorTest extends AbstractEvaluatorTest {
   "def makeObject otherParam (@namedArgs namedArgs) = [otherParam, namedArgs]; makeObject 1 --foo=2" ==> "[1, { foo: 2 }]"
   "def makeObject otherParam (@namedArgs namedArgs) = [otherParam, namedArgs]; makeObject --foo=2 --otherParam=1" ==>
     "[1, { foo: 2 }]"
+  "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject --namedArgs=42" ==> "{ namedArgs: 42 }"
+  "def makeObject varargs... (@namedArgs namedArgs) = { varargs, namedArgs }; makeObject 1 2 3 --foo=42" ==>
+    "{ varargs: [1, 2, 3], namedArgs: { foo: 42} }"
+
   "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject 1" shouldThrowAnException
 
-  "def foo (@namedArgs args) = args; foo --arg --arg" shouldThrowAnException
+  "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject --arg --arg" shouldThrowAnException
 
-  "def foo (@namedArgs args) = args; foo -aa" shouldThrowAnException
+  "def makeObject (@namedArgs namedArgs) = namedArgs; makeObject -aa" shouldThrowAnException
 
-  "def foo arg (@namedArgs args) = args; foo 42 --arg=100" shouldThrowAnException
+  "def makeObject arg (@namedArgs namedArgs) = namedArgs; makeObject 42 --arg=100" shouldThrowAnException
 
   // doc comments
   """# Square a number
