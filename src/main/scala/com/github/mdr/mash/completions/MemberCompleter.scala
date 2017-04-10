6234d5634d965fd19e70c304990380b9ce81559d
Matt Russell <MattRussellUK@gmail.com>
2017-04-10 21:43:27 +0100

    Misc tidying

--- src/main/scala/com/github/mdr/mash/completions/MemberCompleter.scala
+++ src/main/scala/com/github/mdr/mash/completions/MemberCompleter.scala
@@ -1,15 +1,10 @@
 package com.github.mdr.mash.completions
 
-import com.github.mdr.mash.classes.MashClass
 import com.github.mdr.mash.inference.Type
-import com.github.mdr.mash.inference.Type.UserClass
 import com.github.mdr.mash.lexer.Token
-import com.github.mdr.mash.ns.collections.ListClass
-import com.github.mdr.mash.ns.core._
 import com.github.mdr.mash.parser.AbstractSyntax._
 import com.github.mdr.mash.parser.{ ConcreteSyntax, SourceInfo }
-import com.github.mdr.mash.runtime.{ MashObject, MashValue }
-import com.github.mdr.mash.utils.{ Region, StringUtils, Utils }
+import com.github.mdr.mash.utils.{ Region, StringUtils }
 
 import scala.PartialFunction._
 
@@ -29,7 +24,7 @@ object MemberCompleter {
       sourceInfo ← expr.sourceInfoOpt
       tokens = sourceInfo.node.tokens
       identifierToken ← tokens.find(t ⇒ t.isIdentifier && t.region == dummyIdentifierRegion)
-      memberExprOrImport ← findMemberExprOrImport(expr, identifierToken)
+      memberExprOrImport ← findMemberLike(expr, identifierToken)
       target = getTarget(memberExprOrImport)
       members ← getMembers(target)
       completions = members.map(_.asCompletion(isQuoted = false))
@@ -39,33 +34,33 @@ object MemberCompleter {
   }
 
   def completeString(targetType: Type, prefix: String): Seq[Completion] =
-    getMembers(targetType).filter(_.name startsWith prefix).map(_.asCompletion(isQuoted = true))
+    MemberFinder.getMembers(targetType).filter(_.name startsWith prefix).map(_.asCompletion(isQuoted = true))
 
   private def getTarget(expr: Expr) = expr match {
     case memberExpr: MemberExpr           ⇒ memberExpr.target
     case importStatement: ImportStatement ⇒ importStatement.expr
-    case _                                ⇒ throw new AssertionError(s"Unexpected target: $expr")
+    case _                                ⇒ throw new AssertionError(s"Unexpected expression: $expr")
   }
 
   private def getMembers(target: Expr): Option[Seq[MemberInfo]] =
-    target.constantValueOpt.flatMap(getValueMembers) orElse target.typeOpt.map(getMembers(_))
+    target.constantValueOpt.flatMap(MemberFinder.getValueMembers) orElse target.typeOpt.map(MemberFinder.getMembers(_))
 
   def completeIdentifier(text: String, identifier: Token, parser: CompletionParser): MemberCompletionResult = {
-    val exprOpt = findMemberExprOrImport(text, identifier, parser)
+    val memberLikeOpt = findMemberLike(text, identifier, parser)
     val completionResultOpt =
       for {
-        expr ← exprOpt
-        target = getTarget(expr)
+        memberLike ← memberLikeOpt
+        target = getTarget(memberLike)
         targetType ← target.typeOpt
         members ← getMembers(target)
         completions = members.filter(_.name startsWith identifier.text).map(_.asCompletion(isQuoted = false))
         result ← CompletionResult.of(completions, identifier.region)
       } yield result
-    val prioritiseMembers = exprOpt.exists {
+    val prioritiseMembers = memberLikeOpt.exists {
       case memberExpr: MemberExpr ⇒ shouldPrioritise(memberExpr)
       case _                      ⇒ false
     }
-    MemberCompletionResult(exprOpt.isDefined, completionResultOpt, prioritiseMembers)
+    MemberCompletionResult(memberLikeOpt.isDefined, completionResultOpt, prioritiseMembers)
   }
 
   private def shouldPrioritise(memberExpr: MemberExpr): Boolean =
@@ -81,89 +76,19 @@ object MemberCompleter {
       }
     }
 
-  private def findMemberExprOrImport(text: String, identifier: Token, parser: CompletionParser): Option[Expr] = {
+  private def findMemberLike(text: String, identifier: Token, parser: CompletionParser): Option[Expr] = {
     val expr = parser.parse(text)
     for {
       sourceInfo ← expr.sourceInfoOpt
       tokens = sourceInfo.node.tokens
-      memberExpr ← findMemberExprOrImport(expr, identifier)
+      memberExpr ← findMemberLike(expr, identifier)
     } yield memberExpr
   }
 
-  private def findMemberExprOrImport(expr: Expr, token: Token): Option[Expr] = expr.find {
+  private def findMemberLike(expr: Expr, token: Token): Option[Expr] = expr.find {
     case e@MemberExpr(_, _, _, Some(SourceInfo(_, ConcreteSyntax.MemberExpr(_, _, `token`))))           ⇒ e
     case e@MemberExpr(_, _, _, Some(SourceInfo(_, ConcreteSyntax.HeadlessMemberExpr(_, `token`))))      ⇒ e
     case e@ImportStatement(_, _, Some(SourceInfo(_, ConcreteSyntax.ImportStatement(_, _, _, `token`)))) ⇒ e
   }
-
-  private def getMembers(klass: MashClass): Seq[MemberInfo] = {
-    val fieldMembers = klass.fields.map(f ⇒
-      MemberInfo(f.name, classNameOpt = klass.nameOpt, descriptionOpt = f.summaryOpt, isField = true))
-    val methodMembers = klass.methods.flatMap(m ⇒
-      m.names.map(name ⇒ MemberInfo(name, classNameOpt = klass.nameOpt, descriptionOpt = m.summaryOpt, isField = false)))
-    val parentClassMembers = klass.parentOpt.toSeq.flatMap(parentClass ⇒ getMembers(parentClass))
-    distinct(fieldMembers ++ methodMembers ++ parentClassMembers)
-  }
-
-  private def getMembers(userClass: UserClass): Seq[MemberInfo] = {
-    val fieldMembers = userClass.params.params.flatMap(_.nameOpt).map(name ⇒
-      MemberInfo(name, classNameOpt = Some(userClass.name), isField = true))
-    val methodMembers = userClass.methods.collect { case (name, method) if method.isPublic ⇒
-      MemberInfo(name, classNameOpt = Some(userClass.name), isField = false)
-    }
-    val parentClassMembers = getMembers(ObjectClass)
-    distinct(fieldMembers ++ methodMembers ++ parentClassMembers)
-  }
-
-  private def getStaticMembers(userClass: UserClass): Seq[MemberInfo] = {
-    val staticMethodMembers = Seq(MemberInfo("new", classNameOpt = Some(userClass.name), isField = false, isStatic = true))
-    distinct(staticMethodMembers ++ getMembers(ClassClass))
-  }
-
-  private def getValueMembers(klass: MashClass): Seq[MemberInfo] = {
-    val staticMethodMembers = klass.staticMethods.map(method ⇒
-      MemberInfo(method.name, classNameOpt = klass.nameOpt, descriptionOpt = method.summaryOpt, isField = false, isStatic = true))
-    distinct(staticMethodMembers ++ getMembers(ClassClass))
-  }
-
-  private def getValueMembers(obj: MashObject): Seq[MemberInfo] =
-    obj.classOpt match {
-      case Some(klass) ⇒
-        val extraFieldMembers = obj.fields.keys.toSeq.filterNot(klass.fieldsMap.contains).map(MemberInfo(_, isField = true))
-        distinct(extraFieldMembers ++ getMembers(klass))
-      case None        ⇒
-        val fieldMembers = obj.fields.keys.toSeq.map(MemberInfo(_, isField = true))
-        distinct(fieldMembers ++ getMembers(ObjectClass))
-    }
-
-  private def getValueMembers(value: MashValue): Option[Seq[MemberInfo]] =
-    condOpt(value) {
-      case klass: MashClass ⇒ getValueMembers(klass)
-      case obj: MashObject  ⇒ getValueMembers(obj)
-    }
-
-  private def getMembers(type_ : Type,
-                         canVectorise: Boolean = true): Seq[MemberInfo] = type_ match {
-    case Type.Instance(klass)              ⇒ getMembers(klass)
-    case Type.UserClassInstance(userClass) ⇒ getMembers(userClass)
-    case userClass: Type.UserClass         ⇒ getStaticMembers(userClass)
-    case Type.Tagged(baseClass, tagClass)  ⇒ distinct(getMembers(tagClass) ++ getMembers(baseClass))
-    case Type.Generic(klass, _*)           ⇒ getMembers(klass)
-    case Type.BuiltinFunction(_)           ⇒ getMembers(FunctionClass)
-    case Type.BoundBuiltinMethod(_, _)     ⇒ getMembers(BoundMethodClass)
-    case Type.Object(fields)               ⇒
-      val fieldMembers = fields.keys.toSeq.map(f ⇒ MemberInfo(f, isField = true))
-      distinct(fieldMembers ++ getMembers(ObjectClass))
-    case Type.Seq(elementType)             ⇒
-      val seqMembers = getMembers(ListClass)
-      if (canVectorise) {
-        val elementMembers = getMembers(elementType, canVectorise = false).map(_.copy(isVectorised = true))
-        distinct(seqMembers ++ elementMembers)
-      } else
-        seqMembers
-    case _                                 ⇒ Seq()
-  }
-
-  private def distinct(members: Seq[MemberInfo]) = Utils.distinctBy[MemberInfo, String](members, _.name)
 
 }
---
+++ src/main/scala/com/github/mdr/mash/completions/MemberFinder.scala
@@ -0,0 +1,91 @@
+package com.github.mdr.mash.completions
+
+import com.github.mdr.mash.classes.MashClass
+import com.github.mdr.mash.inference.Type
+import com.github.mdr.mash.inference.Type.{ Object, UserClass }
+import com.github.mdr.mash.ns.collections.ListClass
+import com.github.mdr.mash.ns.core.{ BoundMethodClass, ClassClass, FunctionClass, ObjectClass }
+import com.github.mdr.mash.runtime.{ MashObject, MashValue }
+import com.github.mdr.mash.utils.Utils
+
+import scala.PartialFunction._
+
+object MemberFinder {
+
+  def getMembers(targetType: Type, canVectorise: Boolean = true): Seq[MemberInfo] =
+    targetType match {
+      case Type.Instance(klass)              ⇒ getMembers(klass)
+      case Type.UserClassInstance(userClass) ⇒ getMembers(userClass)
+      case userClass: Type.UserClass         ⇒ getStaticMembers(userClass)
+      case Type.Tagged(baseClass, tagClass)  ⇒ distinct(getMembers(tagClass) ++ getMembers(baseClass))
+      case Type.Generic(klass, _*)           ⇒ getMembers(klass)
+      case Type.BuiltinFunction(_)           ⇒ getMembers(FunctionClass)
+      case Type.BoundBuiltinMethod(_, _)     ⇒ getMembers(BoundMethodClass)
+      case objectType: Type.Object           ⇒ getObjectMembers(objectType)
+      case Type.Seq(elementType)             ⇒ getListMembers(elementType, canVectorise)
+      case _                                 ⇒ Seq()
+    }
+
+  private def getObjectMembers(objectType: Object): Seq[MemberInfo] = {
+    val fieldMembers = objectType.knownFields.keys.toSeq.map(f ⇒ MemberInfo(f, isField = true))
+    distinct(fieldMembers ++ getMembers(ObjectClass))
+  }
+
+  private def getListMembers(elementType: Type, canVectorise: Boolean): Seq[MemberInfo] = {
+    val seqMembers = getMembers(ListClass)
+    if (canVectorise) {
+      val elementMembers = getMembers(elementType, canVectorise = false).map(_.copy(isVectorised = true))
+      distinct(seqMembers ++ elementMembers)
+    } else
+      seqMembers
+  }
+
+  private def getMembers(klass: MashClass): Seq[MemberInfo] = {
+    val fieldMembers = klass.fields.map(f ⇒
+      MemberInfo(f.name, classNameOpt = klass.nameOpt, descriptionOpt = f.summaryOpt, isField = true))
+    val methodMembers = klass.methods.flatMap(m ⇒
+      m.names.map(name ⇒ MemberInfo(name, classNameOpt = klass.nameOpt, descriptionOpt = m.summaryOpt, isField = false)))
+    val parentClassMembers = klass.parentOpt.toSeq.flatMap(parentClass ⇒ getMembers(parentClass))
+    distinct(fieldMembers ++ methodMembers ++ parentClassMembers)
+  }
+
+  private def getMembers(userClass: UserClass): Seq[MemberInfo] = {
+    val fieldMembers = userClass.params.params.flatMap(_.nameOpt).map(name ⇒
+      MemberInfo(name, classNameOpt = Some(userClass.name), isField = true))
+    val methodMembers = userClass.methods.collect { case (name, method) if method.isPublic ⇒
+      MemberInfo(name, classNameOpt = Some(userClass.name), isField = false)
+    }
+    val parentClassMembers = getMembers(ObjectClass)
+    distinct(fieldMembers ++ methodMembers ++ parentClassMembers)
+  }
+
+  private def getStaticMembers(userClass: UserClass): Seq[MemberInfo] = {
+    val staticMethodMembers = Seq(MemberInfo("new", classNameOpt = Some(userClass.name), isField = false, isStatic = true))
+    distinct(staticMethodMembers ++ getMembers(ClassClass))
+  }
+
+  def getValueMembers(value: MashValue): Option[Seq[MemberInfo]] =
+    condOpt(value) {
+      case klass: MashClass ⇒ getValueMembers(klass)
+      case obj: MashObject  ⇒ getValueMembers(obj)
+    }
+
+  private def getValueMembers(klass: MashClass): Seq[MemberInfo] = {
+    val staticMethodMembers = klass.staticMethods.map(method ⇒
+      MemberInfo(method.name, classNameOpt = klass.nameOpt, descriptionOpt = method.summaryOpt, isField = false, isStatic = true))
+    distinct(staticMethodMembers ++ getMembers(ClassClass))
+  }
+
+  private def getValueMembers(obj: MashObject): Seq[MemberInfo] =
+    obj.classOpt match {
+      case Some(klass) ⇒
+        val extraFieldMembers = obj.fields.keys.toSeq.filterNot(klass.fieldsMap.contains).map(MemberInfo(_, isField = true))
+        distinct(extraFieldMembers ++ getMembers(klass))
+      case None        ⇒
+        val fieldMembers = obj.fields.keys.toSeq.map(MemberInfo(_, isField = true))
+        distinct(fieldMembers ++ getMembers(ObjectClass))
+    }
+
+  private def distinct(members: Seq[MemberInfo]) = Utils.distinctBy[MemberInfo, String](members, _.name)
+
+}
