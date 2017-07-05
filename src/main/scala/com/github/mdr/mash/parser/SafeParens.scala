c4cbdf6c7304bcd3d7765a7fa84ff4e45a40cbf1
Matt Russell <MattRussellUK@gmail.com>
2017-07-05 19:56:58 +0100

    SafeParens test

--- src/main/scala/com/github/mdr/mash/parser/SafeParens.scala
+++ src/main/scala/com/github/mdr/mash/parser/SafeParens.scala
@@ -1,5 +1,7 @@
 package com.github.mdr.mash.parser
 
+import com.github.mdr.mash.lexer.TokenType
+import com.github.mdr.mash.lexer.TokenType.{ DIVIDE, MINUS, PLUS, TIMES }
 import com.github.mdr.mash.parser.ConcreteSyntax._
 import com.github.mdr.mash.utils.Utils._
 
@@ -7,7 +9,7 @@ object SafeParens {
 
   /**
     * Add parentheses to the given expression if required to allow it to be followed either by a lookup or member expression:
-    *  XXX[1], or XXX.field
+    * XXX[1], or XXX.field
     */
   def safeParens(exprString: String): String = {
     val expr = MashParser.parseForgiving(exprString).body
@@ -22,12 +24,24 @@ object SafeParens {
     val prefixExpr = MashParser.parseForgiving(prefix).body
     val suffixExpr = MashParser.parseForgiving("it" + suffix).body
     val needsParens = (prefixExpr, suffixExpr) match {
-      case (_: ParenExpr | _: MemberExpr | _: LookupExpr | _: BlockExpr | _: ListExpr | _: ObjectExpr | _: Identifier, _) ⇒ false
-      case (_: PipeExpr, _: PipeExpr)                                                                                     ⇒ false
-      case _                                                                                                              ⇒ true
+      case (op1: BinOpExpr, op2: BinOpExpr)                                                                                            ⇒
+        val token1 = op1.op.tokenType
+        val token2 = op2.op.tokenType
+        !(isMultiplicativeOp(token1) && isAdditiveOpt(token2) ||
+          isMultiplicativeOp(token1) && isMultiplicativeOp(token2) ||
+          isAdditiveOpt(token1) && isAdditiveOpt(token2))
+      case (_, _: InvocationExpr)                                                                                                      ⇒ true
+      case (_: ParenExpr | _: MemberExpr | _: LookupExpr | _: BlockExpr | _: ListExpr | _: ObjectExpr | _: Identifier | _: Literal, _) ⇒ false
+      case (_: LambdaExpr, _: PipeExpr)                                                                                                ⇒ true
+      case (_, _: PipeExpr)                                                                                                            ⇒ false
+      case _                                                                                                                           ⇒ true
     }
     prefix.when(needsParens, parenthesise) + suffix
   }
+
+  private def isAdditiveOpt(tokenType: TokenType) = Set[TokenType](PLUS, MINUS) contains tokenType
+
+  private def isMultiplicativeOp(tokenType: TokenType) = Set[TokenType](TIMES, DIVIDE) contains tokenType
 
   private def parenthesise(s: String) = s"($s)"
 
