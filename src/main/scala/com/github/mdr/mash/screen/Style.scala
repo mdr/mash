57bc6ea2e06810109df5244f90c3e172be37ee3b
Matt Russell <MattRussellUK@gmail.com>
2017-07-19 18:28:34 +0100

    Split StyledString from Style

--- src/main/scala/com/github/mdr/mash/screen/Style.scala
+++ src/main/scala/com/github/mdr/mash/screen/Style.scala
@@ -1,7 +1,5 @@
 package com.github.mdr.mash.screen
 
-import com.github.mdr.mash.utils.Utils
-
 object Style {
 
   implicit class StylableString(s: String) {
@@ -27,43 +25,3 @@ case class Style(foregroundColour: Colour = DefaultColour,
                  backgroundColour: Colour = DefaultColour,
                  bold: Boolean = false,
                  inverse: Boolean = false)
-
-case class StyledCharacter(c: Char, style: Style = Style()) {
-
-  def withStyle(style: Style) = copy(style = style)
-
-  def updateStyle(f: Style ⇒ Style) = withStyle(f(style))
-
-}
-
-object StyledString {
-
-  val empty = StyledString(Seq())
-
-  def mkString(strings: Seq[StyledString], separator: StyledString) =
-    StyledString(Utils.intercalate(strings.map(_.chars), separator.chars))
-}
-
-case class StyledString(chars: Seq[StyledCharacter]) {
-
-  def size: Int = chars.size
-
-  def length: Int = chars.size
-
-  def take(n: Int) = copy(chars.take(n))
-
-  def drop(n: Int) = copy(chars.drop(n))
-
-  def apply(i: Int) = chars.apply(i)
-
-  def +(that: StyledString): StyledString = StyledString(this.chars ++ that.chars)
-
-  def +(that: Seq[StyledCharacter]): StyledString = StyledString(this.chars ++ that)
-
-  def updated(i: Int, c: StyledCharacter) = copy(chars.updated(i, c))
-
-  def grouped(n: Int) = chars.grouped(n).map(StyledString(_))
-
-  def isEmpty = chars.isEmpty
-
-}
---
+++ src/main/scala/com/github/mdr/mash/screen/StyledString.scala
@@ -0,0 +1,43 @@
+package com.github.mdr.mash.screen
+
+import com.github.mdr.mash.utils.Utils
+
+case class StyledCharacter(c: Char, style: Style = Style()) {
+
+  def withStyle(style: Style) = copy(style = style)
+
+  def updateStyle(f: Style ⇒ Style) = withStyle(f(style))
+
+}
+
+object StyledString {
+
+  val empty = StyledString(Seq())
+
+  def mkString(strings: Seq[StyledString], separator: StyledString) =
+    StyledString(Utils.intercalate(strings.map(_.chars), separator.chars))
+}
+
+case class StyledString(chars: Seq[StyledCharacter]) {
+
+  def size: Int = chars.size
+
+  def length: Int = chars.size
+
+  def take(n: Int) = copy(chars.take(n))
+
+  def drop(n: Int) = copy(chars.drop(n))
+
+  def apply(i: Int) = chars.apply(i)
+
+  def +(that: StyledString): StyledString = StyledString(this.chars ++ that.chars)
+
+  def +(that: Seq[StyledCharacter]): StyledString = StyledString(this.chars ++ that)
+
+  def updated(i: Int, c: StyledCharacter) = copy(chars.updated(i, c))
+
+  def grouped(n: Int) = chars.grouped(n).map(StyledString(_))
+
+  def isEmpty = chars.isEmpty
+
+}
