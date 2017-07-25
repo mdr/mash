74cdc5c062b3759a9b1dd902a7d2259cdb9fb5ee
Matt Russell <MattRussellUK@gmail.com>
2017-07-25 10:50:49 +0300

    Extra test for incremental history search rendering

--- src/test/scala/com/github/mdr/mash/render/IncrementalHistorySearchRendererTest.scala
+++ src/test/scala/com/github/mdr/mash/render/IncrementalHistorySearchRendererTest.scala
@@ -1,22 +1,38 @@
 package com.github.mdr.mash.render
 
+import java.time.Clock
+import java.time.temporal.ChronoUnit
+
 import com.github.mdr.mash.render.IncrementalHistorySearchRenderer.renderHistorySearchState
 import com.github.mdr.mash.repl.IncrementalHistorySearchState
-import com.github.mdr.mash.repl.IncrementalHistorySearchState.BeforeFirstHit
+import com.github.mdr.mash.repl.IncrementalHistorySearchState._
 import com.github.mdr.mash.terminal.DummyTerminal.SufficientlyLargeTerminalSize
-import com.github.mdr.mash.utils.Point
+import com.github.mdr.mash.utils.{ Point, Region }
 
 class IncrementalHistorySearchRendererTest extends RendererTest {
 
-  "Incremental history search" should "be rendered correctly if there is enough space" in {
-    val state = IncrementalHistorySearchState("searchString", BeforeFirstHit)
+  "Incremental history search" should "render correctly before first hit" in {
+    val state = IncrementalHistorySearchState("", BeforeFirstHit)
 
     val LinesAndCursorPos(Seq(line1, line2), Some(cursorPos)) = renderHistorySearchState(state, SufficientlyLargeTerminalSize)
+
+    val expectedLine1 = "Incremental history search: "
+    getText(line1) shouldEqual expectedLine1
+    cursorPos shouldEqual Point(0, expectedLine1.length)
+    getText(line2) shouldEqual "^R next, ret done"
+  }
+
+  it should "render correctly if a hit is found" in {
+    val oneDayAgo = Clock.systemDefaultZone.instant.minus(24L, ChronoUnit.HOURS)
+    val state = IncrementalHistorySearchState("searchString", Hit(0, Region(0, 1), oneDayAgo, "/etc"))
+
+    val LinesAndCursorPos(Seq(line1, line2, line3), Some(cursorPos)) = renderHistorySearchState(state, SufficientlyLargeTerminalSize)
 
     val expectedLine1 = "Incremental history search: searchString"
     getText(line1) shouldEqual expectedLine1
     cursorPos shouldEqual Point(0, expectedLine1.length)
-    getText(line2) shouldEqual "^R next, ret done"
+    getText(line2) shouldEqual "Hit 1: 1 day ago in /etc"
+    getText(line3) shouldEqual "^R next, ret done"
   }
 
   it should "truncate with ellipses if there is insufficient width, and not display a cursor" in {
