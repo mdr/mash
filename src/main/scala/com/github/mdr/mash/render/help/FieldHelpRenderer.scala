a04e3b5cad59171e7b2612db6e8f607a614d7999
Matt Russell <MattRussellUK@gmail.com>
2017-07-26 21:03:25 +0300

    Refactor FieldHelpRenderer

--- src/main/scala/com/github/mdr/mash/render/help/FieldHelpRenderer.scala
+++ src/main/scala/com/github/mdr/mash/render/help/FieldHelpRenderer.scala
@@ -1,30 +1,39 @@
 package com.github.mdr.mash.render.help
 
 import com.github.mdr.mash.ns.core.help.FieldHelpClass
+import com.github.mdr.mash.ns.core.help.FieldHelpClass.Wrapper
 import com.github.mdr.mash.runtime.MashObject
 import com.github.mdr.mash.screen.Line
 import com.github.mdr.mash.screen.Style._
-
-import scala.collection.mutable.ArrayBuffer
 
 object FieldHelpRenderer {
 
   import HelpRenderer._
 
   def render(obj: MashObject): Seq[Line] = {
-    val lines = ArrayBuffer[Line]()
     val fieldHelp = FieldHelpClass.Wrapper(obj)
-    lines += Line(SectionTitleStyle("FIELD"))
-    lines += Line(IndentSpace + NameStyle(fieldHelp.name) + fieldHelp.summaryOpt.fold("")(" - " + _).style)
-    lines += Line.Empty
-    lines += Line(SectionTitleStyle("CLASS"))
-    lines += Line(IndentSpace + fieldHelp.klass.style)
-    for (description ← fieldHelp.descriptionOpt) {
-      lines += Line.Empty
-      lines += Line(SectionTitleStyle("DESCRIPTION"))
-      lines ++= DescriptionRenderer.render(description)
-    }
-    lines
+    Seq(
+      renderNameSection(fieldHelp),
+      renderClassSection(fieldHelp),
+      renderDescriptionSection(fieldHelp)).flatten
   }
 
+  private def renderNameSection(fieldHelp: Wrapper): Seq[Line] = {
+    val nameLine = Line(IndentSpace + NameStyle(fieldHelp.name) + fieldHelp.summaryOpt.fold("")(" - " + _).style)
+    Seq(
+      Line(SectionTitleStyle("FIELD")),
+      nameLine)
+  }
+
+  private def renderClassSection(fieldHelp: Wrapper): Seq[Line] =
+    Seq(
+      Line.Empty,
+      Line(SectionTitleStyle("CLASS")),
+      Line(IndentSpace + fieldHelp.klass.style))
+
+  private def renderDescriptionSection(fieldHelp: Wrapper): Seq[Line] =
+    fieldHelp.descriptionOpt.toSeq.flatMap(description ⇒
+      Seq(Line.Empty,
+        Line(SectionTitleStyle("DESCRIPTION"))
+      ) ++ DescriptionRenderer.render(description))
 }
