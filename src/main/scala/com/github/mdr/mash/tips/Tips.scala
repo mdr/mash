04ccdef1452389dde11e583c7988c1995a2a6b5f
Matt Russell <MattRussellUK@gmail.com>
2017-08-15 08:43:20 +0100

    extra tips

--- src/main/scala/com/github/mdr/mash/tips/Tips.scala
+++ src/main/scala/com/github/mdr/mash/tips/Tips.scala
@@ -24,13 +24,17 @@ object Tips {
     "Ctrl-r starts an incremental search through history.",
     "Ctrl-q toggles quotation marks around the current region of text.",
     "Alt-. inserts the last argument from history.",
-    "Ctrl-t evaluates the current command, and replaces the line buffer with the result.",
+    "Alt-e evaluates the current command (or selection), and replaces the line buffer (or selection) with the result.",
     "Press tab twice to enter completion browsing mode.",
     "Member accesses on lists can be automatically vectorised: <mash>ls.size.sum</mash>",
     """Strings can be interpolated: <mash>"Hello $user.name"</mash>""",
     s"Suppress tips by setting <mash>config.${Config.Cli.ShowStartupTips} = false</mash>.",
     s"Set <mash>config.${Config.View.FuzzyTime} = true</mash> to view date/times as '3 hours ago', etc.",
-    s"Set <mash>config.${Config.View.BrowseLargeOutput} = false</mash> to avoid starting the browser on long output.")
+    s"Set <mash>config.${Config.View.BrowseLargeOutput} = false</mash> to avoid starting the browser on long output.",
+    "Alt-Up expands the current selection to the nearest enclosing piece of syntax; Alt-Down unexpands.",
+    "Shift-left/right/up/down expands the selection.",
+    "Ctrl-_ undoes the previous edit",
+    "Ctrl-w cuts the current selection, Alt-w copies, and Ctrl-y pastes.")
 
   private def randomTip = Tips(Random.nextInt(Tips.length))
 
