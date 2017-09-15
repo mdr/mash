937d33a8e189fa7d2ae5e26225cb98acdc436c29
Matt Russell <MattRussellUK@gmail.com>
2017-09-15 07:04:31 +0100

    extra key shortcuts

--- src/main/scala/com/github/mdr/mash/input/InputSequenceReader.scala
+++ src/main/scala/com/github/mdr/mash/input/InputSequenceReader.scala
@@ -2,7 +2,6 @@ package com.github.mdr.mash.input
 
 import com.github.mdr.mash.Singletons
 import com.github.mdr.mash.input.KeyDsl._
-import com.github.mdr.mash.input.Key
 import com.github.mdr.mash.utils.PrefixTree
 import com.github.mdr.mash.utils.CharUtils._
 
@@ -18,42 +17,50 @@ object InputSequenceReader {
     makeEscapeTree(
       "^[[1;2A" → shift(Up), // Gnome terminal, xterm, iTerm
       "^[[1;2B" → shift(Down), // Gnome terminal, xterm, iTerm
-      "^[[1;2D" → shift(Left), // Gnome terminal, xterm, Terminal.app, iTerm
       "^[[1;2C" → shift(Right), // Gnome terminal, xterm, Terminal.app, iTerm
+      "^[[1;2D" → shift(Left), // Gnome terminal, xterm, Terminal.app, iTerm
+      "^[[2A" → shift(Up), // intellij
+      "^[[2B" → shift(Down), // intellij
+      "^[[2C" → shift(Right), // intellij
+      "^[[2D" → shift(Left), // intellij
       "^[[a" → shift(Up), // rxvt
       "^[[b" → shift(Down), // rxvt
-      "^[[d" → shift(Left), // rxvt
       "^[[c" → shift(Right), // rxvt
+      "^[[d" → shift(Left), // rxvt
       "^[[1;3A" → alt(Up), // Gnome terminal, xterm
       "^[[1;3B" → alt(Down), // Gnome terminal, xterm
-      "^[^[[A" → alt(Up), // iTerm, rxvt, putty, Terminal.app
-      "^[^[[B" → alt(Down), // iTerm, rxvt, putty, Terminal.app
+      "^[^[[A" → alt(Up), // iTerm, rxvt, putty, Terminal.app, intellij
+      "^[^[[B" → alt(Down), // iTerm, rxvt, putty, Terminal.app, intellij
+      "^[^[[C" → alt(Right), // Terminal.app, intellij
+      "^[^[[D" → alt(Left), // Terminal.app, intellij
       "^[[1;4C" → altShift(Right), // xterm
       "^[[1;4D" → altShift(Left), // xterm
       "^[[1;10C" → altShift(Right), // iTerm
       "^[[1;10D" → altShift(Left), // iTerm
-      "^[^[[C" → altShift(Right), // putty, Terminal.app
-      "^[^[[D" → altShift(Left), // putty, Terminal.app
-      "^[[1;5C" → control(Right), // xterm
-      "^[[1;5D" → control(Left), // xterm
-      "^[[3~" → KeyPress(Delete), // Linux console, xterm, Gnome terminal, putty, Terminal.app
-      "^[[5~" → KeyPress(PageUp), // Linux console, xterm, Gnome terminal, putty
-      "^[[6~" → KeyPress(PageDown), // Linux console, xterm, Gnome terminal, putty
-      "^[[A" → KeyPress(Up), // Linux console, xterm, Gnome terminal, putty, Terminal.app
-      "^[[B" → KeyPress(Down), // Linux console, xterm, Gnome terminal, putty, Terminal.app
-      "^[[C" → KeyPress(Right), // Linux console, xterm, Gnome terminal, putty, Terminal.app
-      "^[[D" → KeyPress(Left), // Linux console, xterm, Gnome terminal, putty, Terminal.app
+      "^[[1;5C" → control(Right), // xterm, Terminal.app, iTerm
+      "^[[1;5D" → control(Left), // xterm, Terminal.app, iTerm
+      "^[[5C" → control(Right), // intellij
+      "^[[5D" → control(Left), // intellij
+      "^[[3~" → KeyPress(Delete), // Linux console, xterm, Gnome terminal, putty, Terminal.app, intellij, iTerm
+      "^[[5~" → KeyPress(PageUp), // Linux console, xterm, Gnome terminal, putty, iTerm, intellij
+      "^[[6~" → KeyPress(PageDown), // Linux console, xterm, Gnome terminal, putty, iTerm, intellij
+      "^[[A" → KeyPress(Up), // Linux console, xterm, Gnome terminal, putty, Terminal.app, intellij
+      "^[[B" → KeyPress(Down), // Linux console, xterm, Gnome terminal, putty, Terminal.app, intellij
+      "^[[C" → KeyPress(Right), // Linux console, xterm, Gnome terminal, putty, Terminal.app, intellij
+      "^[[D" → KeyPress(Left), // Linux console, xterm, Gnome terminal, putty, Terminal.app, intellij
       "^[[Z" → shift(Tab), // xterm, Gnome terminal, putty, Terminal.app, iTerm
       "^[[7$" → shift(Home), // rxvt
       "^[[8$" → shift(End), // rxvt
-      "^[[1;2H" → shift(Home), // Konsole, xterm
-      "^[[1;2F" → shift(End), // Konsole, xterm
+      "^[[1;2H" → shift(Home), // Konsole, xterm, iTerm
+      "^[[1;2F" → shift(End), // Konsole, xterm, iTerm
+      "^[[2H" → shift(Home), // intellij
+      "^[[2F" → shift(End), // intellij
       "^[[1~" → KeyPress(Home), // Linux console, putty
       "^[[4~" → KeyPress(End), // Linux console, putty
       "^[[7~" → KeyPress(Home), // rxvt
       "^[[8~" → KeyPress(End), // rxvt
-      "^[[H" → KeyPress(Home), // gnome-terminal, iTerm, xterm
-      "^[[F" → KeyPress(End), // gnome-terminal, iTerm, xterm
+      "^[[H" → KeyPress(Home), // gnome-terminal, iTerm, xterm, intellij
+      "^[[F" → KeyPress(End), // gnome-terminal, iTerm, xterm, intellij
       "^[OH" → KeyPress(Home), // older Gnome terminal? https://bugzilla.gnome.org/show_bug.cgi?id=600659
       "^[OF" → KeyPress(End), // older Gnome terminal?
       ("^[" + Del) → alt(Backspace),
@@ -67,8 +74,8 @@ object InputSequenceReader {
       "^[e" → alt('e'),
       "^[f" → alt('f'),
       "^[w" → alt('w'),
-      "^[B" → altShift('b'), // terminal.app
-      "^[F" → altShift('f'), // terminal.app
+      "^[B" → altShift('b'), // terminal.app, iterm
+      "^[F" → altShift('f'), // terminal.app, iterm
       "^[\r" → alt(Enter))
   }
 
