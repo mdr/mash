acee1f19cdb972821e78579913cd4ac5d52d8791
Matt Russell <MattRussellUK@gmail.com>
2017-09-20 07:15:55 +0100

    Use InMemoryTerminal in all ScreenDrawerTests

--- src/test/scala/com/github/mdr/mash/screen/ScreenDrawerTest.scala
+++ src/test/scala/com/github/mdr/mash/screen/ScreenDrawerTest.scala
@@ -13,42 +13,39 @@ class ScreenDrawerTest extends FlatSpec with Matchers {
 
   "ScreenDrawer" should "handle an empty screen" in {
     val screen = Screen(lines = Seq(), cursorPosOpt = None, title = Title)
+
     val ScreenDraw(drawString, None) = new ScreenDrawer(SufficientlyLargeTerminalSize).draw(screen)
+
     replaceEscapes(drawString) shouldEqual s"(hide-cursor)(reset)(set-title $Title)"
     val terminal = new InMemoryTerminal(SufficientlyLargeTerminalSize)
-    terminal.print(drawString)
-    terminal.consume()
-    terminal.screen shouldEqual screen
+    terminal.sendInstructions(drawString) shouldEqual screen
   }
 
   it should "handle a single line" in {
     val screen = makeScreen(Seq(Line("abc".style)), cursorPos = Point(0, 3))
-    val ScreenDraw(drawString, None) = new ScreenDrawer(SufficientlyLargeTerminalSize).draw(screen)
+
+    val drawString = new ScreenDrawer(SufficientlyLargeTerminalSize).draw(screen).drawString
+
     replaceEscapes(drawString) shouldEqual
       s"(hide-cursor)(reset)abc(set-title $Title)(show-cursor)"
     val terminal = new InMemoryTerminal(SufficientlyLargeTerminalSize)
-    terminal.print(drawString)
-    terminal.consume()
-    terminal.screen shouldEqual screen
+    terminal.sendInstructions(drawString) shouldEqual screen
   }
 
   it should "handle a single extra character" in {
     val screen1 = makeScreen(Seq(Line("abc".style)), cursorPos = Point(0, 3))
-
     val screen2 = makeScreen(Seq(Line("abcd".style)), cursorPos = Point(0, 4))
 
-    val ScreenDraw(drawString, None) = new ScreenDrawer(SufficientlyLargeTerminalSize).draw(screen2, previousScreenOpt = Some(screen1))
+    val screenDrawer = new ScreenDrawer(SufficientlyLargeTerminalSize)
+    val drawString1 = screenDrawer.draw(screen1).drawString
+    val drawString2 = screenDrawer.draw(screen2, previousScreenOpt = Some(screen1)).drawString
 
-    replaceEscapes(drawString) shouldEqual s"(hide-cursor)(reset)d(show-cursor)"
+    replaceEscapes(drawString1) shouldEqual s"(hide-cursor)(reset)abc(set-title $Title)(show-cursor)"
+    replaceEscapes(drawString2) shouldEqual s"(hide-cursor)(reset)d(show-cursor)"
 
     val terminal = new InMemoryTerminal(SufficientlyLargeTerminalSize)
-    val screenDrawer = new ScreenDrawer(SufficientlyLargeTerminalSize)
-    terminal.print(screenDrawer.draw(screen1).drawString)
-    terminal.consume()
-    terminal.screen shouldEqual screen1
-    terminal.print(drawString)
-    terminal.consume()
-    terminal.screen shouldEqual screen2
+    terminal.sendInstructions(drawString1) shouldEqual screen1
+    terminal.sendInstructions(drawString2) shouldEqual screen2
   }
 
   it should "correctly handle differences between wrapped lines" in {
@@ -57,62 +54,63 @@ class ScreenDrawerTest extends FlatSpec with Matchers {
         Line("  000".style, endsInNewline = false),
         Line("00".style)),
       cursorPos = Point(1, 2))
-
-    val terminalSize = terminalWithColumns(5)
-    val screenDrawer = new ScreenDrawer(terminalSize)
-    val drawString1 = screenDrawer.draw(screen1).drawString
-    replaceEscapes(drawString1) shouldEqual
-      s"(hide-cursor)(reset)  00000(set-title title)(show-cursor)"
-
     val screen2 = makeScreen(
       lines = Seq(
         Line("   00".style, endsInNewline = false),
         Line("000".style)),
       cursorPos = Point(1, 3))
 
-    val ScreenDraw(drawString2, None) = screenDrawer.draw(screen2, previousScreenOpt = Some(screen1))
+    val terminalSize = terminalWithColumns(5)
+    val screenDrawer = new ScreenDrawer(terminalSize)
+
+    val drawString1 = screenDrawer.draw(screen1).drawString
+    val drawString2 = screenDrawer.draw(screen2, previousScreenOpt = Some(screen1)).drawString
+    replaceEscapes(drawString1) shouldEqual
+      s"(hide-cursor)(reset)  00000(set-title title)(show-cursor)"
     replaceEscapes(drawString2) shouldEqual
       s"(hide-cursor)(reset)(cursor-up 1) (cursor-forward 1)00(cursor-forward 1)0(show-cursor)"
 
     val terminal = new InMemoryTerminal(terminalSize)
-    terminal.print(drawString1)
-    terminal.consume()
-    terminal.screen shouldEqual screen1
-    terminal.print(drawString2)
-    terminal.consume()
-    terminal.screen shouldEqual screen2
+    terminal.sendInstructions(drawString1) shouldEqual screen1
+    terminal.sendInstructions(drawString2) shouldEqual screen2
   }
 
   it should "handle drawing a line with a cursor in the last column" in {
-    val screen1 = makeScreen(
-      lines = Seq(Line("123 X".style)),
-      cursorPos = Point(0, 3))
+    val screen1 = makeScreen(lines = Seq(Line("123 X".style)), cursorPos = Point(0, 3))
+    val screen2 = makeScreen(lines = Seq(Line("1234X".style)), cursorPos = Point(0, 4))
+    val terminalSize = terminalWithColumns(5)
+    val screenDrawer = new ScreenDrawer(terminalSize)
 
-    val screen2 = makeScreen(
-      lines = Seq(Line("1234X".style)),
-      cursorPos = Point(0, 4))
+    val drawString1 = screenDrawer.draw(screen1).drawString
+    val drawString2 = screenDrawer.draw(screen2, previousScreenOpt = Some(screen1)).drawString
 
-    val ScreenDraw(drawString, None) = new ScreenDrawer(terminalWithColumns(5)).draw(screen2, previousScreenOpt = Some(screen1))
+    replaceEscapes(drawString1) shouldEqual s"(hide-cursor)(reset)123 X(carriage-return)(cursor-forward 3)(set-title title)(show-cursor)"
+    replaceEscapes(drawString2) shouldEqual s"(hide-cursor)(reset)4(show-cursor)"
 
-    replaceEscapes(drawString) shouldEqual
-      s"(hide-cursor)(reset)4(show-cursor)"
+    val terminal = new InMemoryTerminal(terminalSize)
+    terminal.sendInstructions(drawString1) shouldEqual screen1
+    terminal.sendInstructions(drawString2) shouldEqual screen2
   }
 
   it should "not reset if only changing colour" in {
     val screen = makeScreen(Seq(
       Line(
         "Y".style(foregroundColour = BasicColour.Yellow) +
-          "G".style(foregroundColour = BasicColour.Green))), cursorPos = Point(0, 2))
-    val ScreenDraw(drawString, None) = new ScreenDrawer(SufficientlyLargeTerminalSize).draw(screen)
+          "G".style(foregroundColour = BasicColour.Green))),
+      cursorPos = Point(0, 2))
+    val screenDrawer = new ScreenDrawer(SufficientlyLargeTerminalSize)
+
+    val drawString = screenDrawer.draw(screen).drawString
+
     replaceEscapes(drawString) shouldEqual
       s"(hide-cursor)(reset)(yellow)Y(green)G(set-title $Title)(show-cursor)"
-
+    val terminal = new InMemoryTerminal(SufficientlyLargeTerminalSize)
+    terminal.sendInstructions(drawString) shouldEqual screen
   }
 
   private def makeScreen(lines: Seq[Line], cursorPos: Point) = Screen(lines, Some(cursorPos), Title)
 
-  private def terminalWithColumns(numberOfColumns: Int) =
-    SufficientlyLargeTerminalSize.withColumns(numberOfColumns)
+  private def terminalWithColumns(numberOfColumns: Int) = SufficientlyLargeTerminalSize.withColumns(numberOfColumns)
 
   private def replaceEscapes(s: String): String = {
     val replacements = Map(
--- src/test/scala/com/github/mdr/mash/terminal/InMemoryTerminal.scala
+++ src/test/scala/com/github/mdr/mash/terminal/InMemoryTerminal.scala
@@ -2,20 +2,23 @@ package com.github.mdr.mash.terminal
 
 import java.io.{ ByteArrayOutputStream, PrintStream }
 
-import com.github.mdr.mash.screen._
 import com.github.mdr.mash.screen.Style._
+import com.github.mdr.mash.screen._
 import com.github.mdr.mash.utils.CharUtils.Esc
-import com.github.mdr.mash.utils.{ Dimensions, Point }
 import com.github.mdr.mash.utils.Utils._
+import com.github.mdr.mash.utils.{ Dimensions, Point }
 
 import scala.collection.mutable.ArrayBuffer
 
+/**
+  * In-memory display-only terminal emulator
+  */
 class InMemoryTerminal(size: Dimensions) {
 
   private val lines = ArrayBuffer[ArrayBuffer[StyledCharacter]]()
   private var wrappedLineIndices: Set[Int] = Set()
 
-  val outputStream = new ByteArrayOutputStream
+  private val outputStream = new ByteArrayOutputStream
 
   private var cursorPos = Point(0, 0)
   private var cursorVisible = true
@@ -31,10 +34,16 @@ class InMemoryTerminal(size: Dimensions) {
   private val MoveCursorToTopLeft = s"(?s)$Esc\\[H(.*)".r
   private val SingleChar = "(?s)(.)(.*)".r
   private val SetTitle = s"(?s)$Esc]0;(.+?)\u0007(.*)".r
+  private val Green = s"(?s)$Esc\\[32m(.*)".r
+  private val Yellow = s"(?s)$Esc\\[33m(.*)".r
 
-  def print(s: String) = new PrintStream(outputStream).print(s)
+  def sendInstructions(s: String): Screen = {
+    new PrintStream(outputStream).print(s)
+    consume()
+    screen
+  }
 
-  def consume() {
+  private def consume() {
     var contents = outputStream.toString
     outputStream.reset()
     while (contents.nonEmpty)
@@ -60,6 +69,12 @@ class InMemoryTerminal(size: Dimensions) {
         case Reset(rest)                     ⇒
           style = Style.Default
           contents = rest
+        case Green(rest)                     ⇒
+          style = style.withForegroundColour(BasicColour.Green)
+          contents = rest
+        case Yellow(rest)                    ⇒
+          style = style.withForegroundColour(BasicColour.Yellow)
+          contents = rest
         case MoveCursorToTopLeft(rest)       ⇒
           cursorPos = Point(0, 0)
           contents = rest
@@ -71,31 +86,33 @@ class InMemoryTerminal(size: Dimensions) {
           cursorPos = cursorPos.copy(column = 0)
           contents = s.tail
         case SingleChar(c, rest)             ⇒
-          val writePos =
-            if (cursorPos.column == size.columns) {
-              wrappedLineIndices += cursorPos.row
-              cursorPos.down().copy(column = 0)
-            } else
-              cursorPos
-          val diff = writePos.row + 1 - lines.size
-          if (diff > 0) {
-            val newLines = List.fill(diff)(ArrayBuffer[StyledCharacter]())
-            lines ++= newLines
-          }
-          val line = lines(writePos.row)
-          val diff2 = writePos.column + 1 - line.length
-          if (diff2 > 0)
-            line ++= List.fill(diff2)(' '.style)
-          line(writePos.column) = c.head.style(style)
-          cursorPos = writePos.right()
+          addSingleChar(c)
           contents = rest
         case s if s.startsWith(Esc.toString) ⇒
-          throw new IllegalArgumentException(s"Unknown escape sequence: ${makeReadable(s)}")
+          val readable = s.replaceAll(Esc.toString, "^[").replaceAll("\r", "\\r").replaceAll("\n", "\\n")
+          throw new IllegalArgumentException(s"Unknown escape sequence: $readable")
       }
   }
 
-  private def makeReadable(s: String) =
-    s.replaceAll(Esc.toString, "^[").replaceAll("\r", "\\r").replaceAll("\n", "\\n")
+  private def addSingleChar(c: String) {
+    val writePos =
+      if (cursorPos.column == size.columns) {
+        wrappedLineIndices += cursorPos.row
+        cursorPos.down().copy(column = 0)
+      } else
+        cursorPos
+    val diff = writePos.row + 1 - lines.size
+    if (diff > 0) {
+      val newLines = List.fill(diff)(ArrayBuffer[StyledCharacter]())
+      lines ++= newLines
+    }
+    val line = lines(writePos.row)
+    val diff2 = writePos.column + 1 - line.length
+    if (diff2 > 0)
+      line ++= List.fill(diff2)(' '.style)
+    line(writePos.column) = c.head.style(style)
+    cursorPos = writePos.right()
+  }
 
   def screen: Screen = {
     val cursorPosOpt = cursorVisible.option(cursorPos)
